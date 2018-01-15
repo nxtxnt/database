7942b78a6499db9c359ad1d58013ec440aa06e51
nxtxnt <laurentengelbrecht1@gmail.com>
2018-01-15 14:58:19 +0100

    Update error.cpp

--- main/error.cpp
+++ main/error.cpp
@@ -3,5 +3,9 @@
 int error(int err_argv) {
   switch(err_argv) {
     case TOO_MANY_ARGV :
-      
+      write(STDIN, "Error : Too many arguments given\n", 34);
+    case SCRIPT_MISSING :
+      write(STDIN, "Error : Script Missing\n", 24);
+    default :
+      break;
 }
