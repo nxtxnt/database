5f379973cfb25fc7a5688f860dba3c56d940f0e1
nxtxnt <laurentengelbrecht1@gmail.com>
2018-01-15 15:02:35 +0100

    Update global.h

--- main/global.h
+++ main/global.h
@@ -3,5 +3,18 @@
 #ifdef _linux_
   #define STDIN 0
 
+#include <unistd.h>
+#include <fcntl.h>
+
 #define FAILURE -1
 #define SUCCESS 0
+
+#ifndef ERROR_HEADER
+#define ERROR_HEADER
+
+int error(int err_arg);
+
+#endif
+
+#define TOO_MANY_ARG -1
+#define SCRIPT_MISSING -2
