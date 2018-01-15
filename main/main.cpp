cb523048d8b3b75607410f1bb34b417c56d5647c
nxtxnt <laurentengelbrecht1@gmail.com>
2018-01-15 15:10:06 +0100

    Update main.cpp

--- main/main.cpp
+++ main/main.cpp
@@ -2,10 +2,11 @@
 #include "global.h"
 
 int main(int argc, char *argv[]) {
+  bool script;
   if(argc < 2)
     error(TOO_MANY_ARG);
   else if(argc == 2)
-    if((scriptHandler(argv[argc]) < 0)
+    if((loadDataBase(argv[argc], script = TRUE) < 0)
        return FAILURE;
   else
     if(loadDatabase() < 0)
