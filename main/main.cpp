94555633d973d90b65d9411aa67f0331c80a4350
nxtxnt <laurentengelbrecht1@gmail.com>
2018-01-15 14:59:50 +0100

    Update main.cpp

--- main/main.cpp
+++ main/main.cpp
@@ -1,11 +1,14 @@
 #include "main.h"
+#include "global.h"
 
 int main(int argc, char *argv[]) {
   if(argc < 2)
     error(TOO_MANY_ARG);
-  if(argc == 2)
-    scriptHandler(argv[argc]);
+  else if(argc == 2)
+    if((scriptHandler(argv[argc]) < 0)
+       return FAILURE;
   else
-    loadDatabase();
+    if(loadDatabase() < 0)
+       return FAILURE;
 }
     
