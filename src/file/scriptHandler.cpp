1d90b769a0f3199a285e9a61fff7a53917428918
nxtxnt <laurentengelbrecht1@gmail.com>
2018-01-17 16:17:10 +0100

    Update scriptHandler.cpp

--- src/file/scriptHandler.cpp
+++ src/file/scriptHandler.cpp
@@ -1,4 +1,21 @@
 #include "scriptHandler.h"
 
 int scriptHander(char *fileName, struct file &dtb) {
+  char *line;
+  char buf;
+  int checkIfChar(0);                                       //checks whether the file is terminated or not
+  int fd;
+  if((fd = open(filename, O_RDONLY)) < 0)
+    error(NULL);
+  int i = 0;
+  while(checkIfChar < 0)
+    checkIfChar = read(fd, *buf, 1);
+    if(buf == '\n') {
+      i = 0;
+      int lineInterpreter(char *line, strut file &dtb);
+    }
+    else {
+      line[i] = buf;
+      i++;
+    }
 }
