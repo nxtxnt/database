dd399d5259dc4d574140bdf703dbb67fb07bb338
nxtxnt <laurentengelbrecht1@gmail.com>
2018-01-17 10:58:45 +0100

    Update error.cpp

--- main/error.cpp
+++ main/error.cpp
@@ -5,7 +5,14 @@ int error(int err_argv) {
     case TOO_MANY_ARGV :
       write(STDOUT, "Error : Too many arguments given\n", 34);
     case SCRIPT_MISSING :
-      write(STDOUT, "Error : Script Missing\n", 24);
+      write(STDOUT, "Error : Script missing\n", 24);
+    case ERROR_MAKING_DATABASE :
+      write(STDOUT, "Error : Could not make database\n", 33);
+    case FSEEK_ERROR :
+      write(STDOUT, "Error : Could not seek requested position in file\n", 51);
+    case FREAD_ERROR :
+      write(STDOUT, "Error : Could not read file\n", 29);
     default :
+      write(STDOUT, "Error\n" 7);
       break;
 }
