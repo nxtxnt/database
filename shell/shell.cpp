67adfeb82237853d7dfc4aa39d8817fadd6913c0
nxtxnt <laurentengelbrecht1@gmail.com>
2018-01-17 09:43:10 +0100

    Update shell.cpp

--- shell/shell.cpp
+++ shell/shell.cpp
@@ -3,3 +3,17 @@
 int shell(file &dtb) {
 
 }
+
+int input(char *line[]) {
+  char buf(0);
+  for(int i = 0; buf != '\n'; i++) {
+    if(read(STDIN, *buf, 1) < 0) {
+      error(ERROR_READING_INPUT);
+      return FAILURE;
+    }
+    line[i] = buf;
+  }
+  return SUCCESS;
+}
+
+  
