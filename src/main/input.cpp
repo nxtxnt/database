a4417e70023e4fcda84a3c283cf6afc11e217c04
nxtxnt <laurentengelbrecht1@gmail.com>
2018-01-20 17:30:42 +0100

    Update input.cpp

--- src/main/input.cpp
+++ src/main/input.cpp
@@ -1,4 +1,13 @@
 #include "global.h"
 
 int input(char *in[]) {
+  char cBuf = NULL;
+  for(int i = 0; cBuf != '\n'; i++) {
+    if((read(STDIN, cBuf, 1)) < 0) {
+      error(FREAD_ERROR);
+      return FAILURE;
+    }
+    in[i] = cBuf;
+  }
+  return SUCCESS;
 }
