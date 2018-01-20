09247d8e0997e7475a1f68137c2f5bd0f9b2cc43
nxtxnt <laurentengelbrecht1@gmail.com>
2018-01-20 13:31:46 +0100

    Update functional.h

--- src/functional/functional.h
+++ src/functional/functional.h
@@ -1,6 +1,10 @@
-#ifndef FUNCTIONAL_HEADER
-#define FUNCTIONAL_HEADER
+#include ../main/global.h"
 
+#ifndef LINE_FUNCTIONS
+#define LINE_FUNCTIONS
 
+int addLine(char *lineName, struct file &dtb);
+int renameLine(char *nwLineName, struct file &dtb);
+int removeLine(char *lineName, struct file &dtb);
 
 #endif
