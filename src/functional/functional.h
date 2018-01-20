d056cb888d8f17a44a41aaa469802b9f65aba071
nxtxnt <laurentengelbrecht1@gmail.com>
2018-01-20 13:41:26 +0100

    Update functional.h

--- src/functional/functional.h
+++ src/functional/functional.h
@@ -8,6 +8,7 @@ int seekLine(char *lineName, struct file *dtb);
 int buffer(int *lineName, char *buffer[], struct file *dtb);
 int erase(int *lineName, struct file *dtb);
 int writeInFile(char *line, char *buffer, struct file *dtb);
+void updateStruct(struct file *dtb, int newLineSize);
 
 #endif
 
