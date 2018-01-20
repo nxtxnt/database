b00fc753941d7626ebd9899ad7c4749b1bcaabaf
nxtxnt <laurentengelbrecht1@gmail.com>
2018-01-20 13:39:57 +0100

    Update functional.h

--- src/functional/functional.h
+++ src/functional/functional.h
@@ -1,10 +1,21 @@
 #include ../main/global.h"
 
+#ifndef FILE_FUNCTIONS
+#define FILE_FUNCTIONS
+
+int hashFunction(char *lineName);
+int seekLine(char *lineName, struct file *dtb);
+int buffer(int *lineName, char *buffer[], struct file *dtb);
+int erase(int *lineName, struct file *dtb);
+int writeInFile(char *line, char *buffer, struct file *dtb);
+
+#endif
+
 #ifndef LINE_FUNCTIONS
 #define LINE_FUNCTIONS
 
-int addLine(char *lineName, struct file &dtb);
-int renameLine(char *nwLineName, struct file &dtb);
-int removeLine(char *lineName, struct file &dtb);
+int addLine(char *lineName, struct file *dtb);
+int renameLine(char *nwLineName, struct file *dtb);
+int removeLine(char *lineName, struct file *dtb);
 
 #endif
