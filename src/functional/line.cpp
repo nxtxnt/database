3c5fd506ad451d59356ca0ebbfd661f854267d11
nxtxnt <laurentengelbrecht1@gmail.com>
2018-01-20 13:32:34 +0100

    Create line.cpp

---
+++ src/functional/line.cpp
@@ -0,0 +1,13 @@
+#include "functional.h"
+
+int addLine(char *lineName, struct file &dtb) {
+
+}
+
+int renameLine(char *nwLineName, struct file &dtb) {
+
+}
+
+int removeLine(char *lineName, struct file &dtb) {
+
+}
