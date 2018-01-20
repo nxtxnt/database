0632708ea16b0f07120913cff19b1090266284ee
nxtxnt <laurentengelbrecht1@gmail.com>
2018-01-20 17:37:17 +0100

    Update line.cpp

--- src/functional/line.cpp
+++ src/functional/line.cpp
@@ -1,7 +1,15 @@
 #include "functional.h"
 
-int addLine(char *lineName, struct file &dtb) {
-
+int addLine(char *lineName, char *line, struct file &dtb) {
+  char *buffer;
+  seekLine(lineName, &dtb);
+  buffer(lineName, &buffer, &dtb);
+  seekLine(lineName, &dtb);
+  erase(lineName, &dtb);
+  seekLine(lineName, &dtb);
+  writeInFile(line, buffer, &dtb);
+  updateStruct(&dtb, sizeof(line));
+  clearArray(&buffer);
 }
 
 int renameLine(char *nwLineName, struct file &dtb) {
